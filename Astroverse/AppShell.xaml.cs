ea266f2521affb65d9d087e1cdfafd0a75e137d7
Daniel Monettelli <danielmonetelli@hotmail.com>
2024-03-23 10:26:01 -0500

    🎉 [Code] Added initial .NET MAUI template

---
+++ Astroverse/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Astroverse
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Astroverse/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Astroverse
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
