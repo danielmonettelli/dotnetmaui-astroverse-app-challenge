aa5dec059ace044deb01535d5f9863685325b75d
Daniel Monettelli <danielmonetelli@hotmail.com>
2024-03-25 11:42:06 -0500

    🚀 [Code] Added "Planet" as a model

---
+++ Astroverse/Models/Planet.cs
@@ -0,0 +1,15 @@
+﻿namespace Astroverse.Models;
+
+public class Planet
+{
+    public int Id_Planet { get; set; }
+    public string Name_Planet { get; set; }
+    public string Meaning_Planet { get; set; }
+    public string Image_Planet { get; set; }
+    public int Temperature_Planet { get; set; }
+    public string Distance_from_Earth_Planet { get; set; }
+    public float Average_Orbital_Speed_Planet { get; set; }
+    public int Satellites_Planet { get; set; }
+    public long Surface_Area_Planet { get; set; }
+    public float Rotation_Period_Planet { get; set; }
+}
